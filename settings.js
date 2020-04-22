8d79acbf08c8fb3510347e4d7e73dbb678af2158
Toni Fisler <mail@toni.io>
2020-04-22 17:26:53 +0200

    fix custom attributes not working for other systems than dnd5e
    Because of some hardcoded stuff, only dnd5e was working. It should be
    fixed now.

---
+++ settings.js
@@ -0,0 +1,100 @@
+import {i18n} from './ui.js';
+
+const MODULE_NAME = 'token-health';
+
+export const CONFIG = {};
+const DEFAULT = {
+  TOGGLE_KEY: 'Enter',
+  HITPOINTS_ATTRIBUTE: '',
+  MAX_HITPOINTS_ATTRIBUTE: '',
+  TEMP_HITPOINTS_ATTRIBUTE: '',
+};
+
+/**
+ * Set all default settings, based on game system
+ */
+const setDefaults = () => {
+  // Default to system values
+  if (game.system.id === 'dnd5e') {
+    DEFAULT.HITPOINTS_ATTRIBUTE = 'attributes.hp.value';
+    DEFAULT.MAX_HITPOINTS_ATTRIBUTE = 'attributes.hp.max';
+    DEFAULT.TEMP_HITPOINTS_ATTRIBUTE = 'attributes.hp.temp';
+  } else {
+    DEFAULT.HITPOINTS_ATTRIBUTE = 'health.value';
+    DEFAULT.MAX_HITPOINTS_ATTRIBUTE = 'health.max';
+  }
+};
+
+/**
+ * Sets the settings or returns the current value.
+ *
+ * @param key
+ * @param setting
+ * @returns {*}
+ */
+const initSetting = (key, setting) => {
+  let config;
+
+  try {
+    config = game.settings.get(MODULE_NAME, key);
+  } catch (e) {
+    if (e.message === 'This is not a registered game setting') {
+      game.settings.register(MODULE_NAME, key, setting);
+      config = game.settings.get(MODULE_NAME, key);
+    } else {
+      throw e;
+    }
+  }
+
+  return config;
+};
+
+/**
+ * Register settings
+ */
+export default () => {
+  setDefaults();
+
+  CONFIG.TOGGLE_KEY = initSetting('toggleKey', {
+    name: i18n('TOKEN_HEALTH.toggleKeyName'),
+    hint: i18n('TOKEN_HEALTH.toggleKeyHint'),
+    type: window.Azzu.SettingsTypes.KeyBinding,
+    default: DEFAULT.TOGGLE_KEY,
+    scope: 'user',
+    config: true,
+    onChange: key => {
+      CONFIG.TOGGLE_KEY = key;
+    },
+  });
+  CONFIG.HITPOINTS_ATTRIBUTE = initSetting('hpSource', {
+    name: i18n('TOKEN_HEALTH.hp'),
+    type: String,
+    default: DEFAULT.HITPOINTS_ATTRIBUTE,
+    scope: 'world',
+    config: true,
+    onChange: key => {
+      CONFIG.HITPOINTS_ATTRIBUTE = key;
+    },
+  });
+  CONFIG.MAX_HITPOINTS_ATTRIBUTE = initSetting('hpSourceMax', {
+    name: i18n('TOKEN_HEALTH.hpMax'),
+    type: String,
+    default: DEFAULT.MAX_HITPOINTS_ATTRIBUTE,
+    scope: 'world',
+    config: true,
+    onChange: key => {
+      CONFIG.MAX_HITPOINTS_ATTRIBUTE = key;
+    },
+  });
+  CONFIG.TEMP_HITPOINTS_ATTRIBUTE = initSetting('tempHpSource', {
+    name: i18n('TOKEN_HEALTH.tempHp'),
+    hint: i18n('TOKEN_HEALTH.tempHpHint'),
+    type: String,
+    default: DEFAULT.TEMP_HITPOINTS_ATTRIBUTE,
+    scope: 'world',
+    config: true,
+    onChange: key => {
+      CONFIG.TEMP_HITPOINTS_ATTRIBUTE = key;
+    },
+  });
+};
--- token-health.js
+++ token-health.js
@@ -1,24 +1,12 @@
 // @ts-check
 
-const DEFAULT = {
-  TOGGLE_KEY: 'Enter',
-  HITPOINTS_ATTRIBUTE: 'attributes.hp.value',
-  TEMP_HITPOINTS_ATTRIBUTE: 'attributes.hp.temp',
-};
+import settings, {CONFIG} from './settings.js';
+import {i18n} from './ui.js';
 
 const DELAY = 400;
-const CONFIG = {...DEFAULT};
 
 let tokenHealthDisplayed = false;
 let dialog, timer;
-
-/**
- * Shorthand, thanks minor-qol =)
- *
- * @param {string} key The translation key
- * @returns {String|string} The translated string
- */
-const i18n = key => game.i18n.localize(key);
 
 /**
  * Extend Dialog class to force focus on the input
@@ -47,20 +35,29 @@ const applyDamage = async (html, isDamage) => {
   const damage = isDamage ? Number(value) : value * -1;
   const tokens = canvas.tokens.controlled;
 
-  const promises = tokens.map(({actor}) => {
+  const promises = tokens.map(({id, actor}) => {
     // Handle temp hp if any
-    const hp = actor.data.data.attributes.hp,
-      tmp = parseInt(hp.temp) || 0,
+    const data = actor.data.data;
+    const hp = getProperty(data, CONFIG.HITPOINTS_ATTRIBUTE);
+    const max = getProperty(data, CONFIG.MAX_HITPOINTS_ATTRIBUTE);
+    const temp = getProperty(data, CONFIG.TEMP_HITPOINTS_ATTRIBUTE);
+
+    const tmp = parseInt(temp) || 0,
       dt = damage > 0 ? Math.min(tmp, damage) : 0;
 
     const newTempHP = tmp - dt;
-    const newHP = Math.clamped(hp.value - (damage - dt), 0, hp.max);
+    const newHP = Math.clamped(hp - (damage - dt), 0, max);
+
+    const updates = {_id: actor.id, isToken: actor.isToken};
+
+    if (CONFIG.HITPOINTS_ATTRIBUTE)
+      updates[`data.${CONFIG.HITPOINTS_ATTRIBUTE}`] = newHP;
+
+    if (CONFIG.TEMP_HITPOINTS_ATTRIBUTE)
+      updates[`data.${CONFIG.TEMP_HITPOINTS_ATTRIBUTE}`] = newTempHP;
 
     // Prepare the update
-    return actor.update({
-      [`data.${CONFIG.TEMP_HITPOINTS_ATTRIBUTE}`]: newTempHP,
-      [`data.${CONFIG.HITPOINTS_ATTRIBUTE}`]: newHP,
-    });
+    return actor.update(updates);
   });
 
   return Promise.all(promises);
@@ -159,44 +156,6 @@ Hooks.once('ready', () => {
     cached_onEscape.call(this, ...arguments);
   };
 
-  // Register our settings
-  game.settings.register('token-health', 'toggleKey', {
-    name: i18n('TOKEN_HEALTH.toggleKeyName'),
-    hint: i18n('TOKEN_HEALTH.toggleKeyHint'),
-    type: window.Azzu.SettingsTypes.KeyBinding,
-    default: DEFAULT.TOGGLE_KEY,
-    scope: 'user',
-    config: true,
-    onChange: key => {
-      CONFIG.TOGGLE_KEY = key;
-    },
-  });
-  game.settings.register('token-health', 'hpSource', {
-    name: i18n('TOKEN_HEALTH.hp'),
-    type: String,
-    default: DEFAULT.HITPOINTS_ATTRIBUTE,
-    scope: 'world',
-    config: true,
-    onChange: key => {
-      CONFIG.HITPOINTS_ATTRIBUTE = key;
-    },
-  });
-  game.settings.register('token-health', 'tempHpSource', {
-    name: i18n('TOKEN_HEALTH.tempHp'),
-    type: String,
-    default: DEFAULT.TEMP_HITPOINTS_ATTRIBUTE,
-    scope: 'world',
-    config: true,
-    onChange: key => {
-      CONFIG.TEMP_HITPOINTS_ATTRIBUTE = key;
-    },
-  });
-
-  // Store the settings immediately
-  CONFIG.TOGGLE_KEY = game.settings.get('token-health', 'toggleKey');
-  CONFIG.HITPOINTS_ATTRIBUTE = game.settings.get('token-health', 'hpSource');
-  CONFIG.TEMP_HITPOINTS_ATTRIBUTE = game.settings.get(
-    'token-health',
-    'tempHpSource'
-  );
+  // Initialize settings
+  settings();
 });
