3744f8c57b3acee445643d70dc09eac2d3300efb
Toni Fisler <mail@toni.io>
2020-04-22 17:26:53 +0200

    fix custom attributes not working for other systems than dnd5e, close #4
    Because of some hardcoded stuff, only dnd5e was working. It should be
    fixed now.

---
+++ ui.js
@@ -0,0 +1,7 @@
+/**
+ * Shorthand, thanks minor-qol =)
+ *
+ * @param {string} key The translation key
+ * @returns {String|string} The translated string
+ */
+export const i18n = key => game.i18n.localize(key);
